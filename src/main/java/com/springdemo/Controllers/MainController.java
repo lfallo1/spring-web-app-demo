ab5bbb633e16e602780f06dafa29055b3d40634c
Lance Fallon <lancefallon@Lances-MacBook-Pro.local>
2015-10-05 21:07:35 -0400

    initial commit

---
+++ src/main/java/com/springdemo/Controllers/MainController.java
@@ -0,0 +1,15 @@
+package com.springdemo.Controllers;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+    @RequestMapping (value = "/", method = RequestMethod.GET)
+    public String mainGET(HttpSession session) {
+        return "home";
+    }
+}
